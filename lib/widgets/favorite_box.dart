5f5235d20b1f7a449f0449c09eb09cd4a6bbef4d
wahyu <wendymarvel4@gmail.com>
2022-06-14 16:45:54 +0700

    revisi favorite box

--- lib/widgets/favorite_box.dart
+++ lib/widgets/favorite_box.dart
@@ -1,46 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:finalproject_pmoif20a_nurhakim/theme/color.dart';
+import 'package:finalproject_pmoif20a_nurhakim/widgets/favorite_box.dart';
+import 'custom_image.dart';
 
-class IconBox extends StatelessWidget {
-  IconBox(
+class FeatureItem extends StatelessWidget {
+  FeatureItem(
       {Key? key,
-        required this.child,
-        this.bgColor = Colors.white,
+        required this.data,
+        this.width = 280,
+        this.height = 300,
         this.onTap,
-        this.borderColor = Colors.transparent,
-        this.radius = 50,
-        this.padding = 5,
-        this.isShadow = true})
+        this.onTapFavorite})
       : super(key: key);
-  final Widget child;
-  final Color borderColor;
-  final Color bgColor;
-  final double radius;
-  final double padding;
+  final data;
+  final double width;
+  final double height;
+  final GestureTapCallback? onTapFavorite;
   final GestureTapCallback? onTap;
-  final bool isShadow;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: EdgeInsets.all(padding),
+        width: width,
+        height: height,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(bottom: 5, top: 5),
         decoration: BoxDecoration(
-          color: bgColor,
-          borderRadius: BorderRadius.circular(radius),
-          border: Border.all(color: borderColor),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
           boxShadow: [
-            if (isShadow)
-              BoxShadow(
-                color: shadowColor.withOpacity(0.1),
-                spreadRadius: 1,
-                blurRadius: 1,
-                offset: Offset(0, 1), // changes position of shadow
-              ),
+            BoxShadow(
+              color: shadowColor.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 1,
+              offset: Offset(1, 1), // changes position of shadow
+            ),
           ],
         ),
-        child: child,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomImage(
+              data["image"],
+              width: double.infinity,
+              height: 190,
+              radius: 15,
+            ),
+            Container(
+              width: width - 20,
+              padding: EdgeInsets.fromLTRB(5, 10, 5, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    data["name"],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: textColor,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data["type"],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: labelColor, fontSize: 13),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            data["price"],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                color: primary,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                      FavoriteBox(
+                        size: 16,
+                        onTap: onTapFavorite,
+                        isFavorited: data["is_favorited"],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
