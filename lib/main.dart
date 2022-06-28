be5deff1138a9cdc4fbe502885c3dcabedd0707a
syalafudin <syalafudinf@gmail.com>
2022-06-28 12:59:16 +0700

    revisi icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'SignIn.dart';
+import 'SignUp.dart';
 import 'screens/root_app.dart';
 import 'theme/color.dart';
+import 'package:firebaselogin/SignIn.dart';
+import 'package:firebaselogin/SignUp.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'HomePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +16,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hotel Booking',
       theme: ThemeData(
         primaryColor: primary,
       ),
-      home: RootApp(),
+
+      home: const RootApp(),
+      routes: {
+        '/home': (BuildContext context) => HomePage(),
+        '/signin': (BuildContext context) => SignIn(),
+        '/signup': (BuildContext context) => SignUp()
+      },
     );
   }
 }
