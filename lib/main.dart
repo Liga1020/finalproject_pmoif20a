aa967b1339355b0e106fadac3e46cab82b8c2b78
syalafudin <syalafudinf@gmail.com>
2022-06-28 13:24:13 +0700

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,17 @@
+import 'package:coffee_shop/pages/front_page.dart';
+import 'package:coffee_shop/routes/app_routes.dart';
 import 'package:flutter/material.dart';
-import 'SignIn.dart';
-import 'SignUp.dart';
-import 'screens/root_app.dart';
-import 'theme/color.dart';
-import 'package:firebaselogin/SignIn.dart';
-import 'package:firebaselogin/SignUp.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'HomePage.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hotel Booking',
-      theme: ThemeData(
-        primaryColor: primary,
-      ),
-
-      home: const RootApp(),
-      routes: {
-        '/home': (BuildContext context) => HomePage(),
-        '/signin': (BuildContext context) => SignIn(),
-        '/signup': (BuildContext context) => SignUp()
-      },
+      home: FrontPage(),
+      getPages: AppRoutes.pages,
     );
   }
 }
